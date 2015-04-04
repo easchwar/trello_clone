698cfec77803b06eaaed774aeeccd032572ceef6
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-03 17:47:28 -0700

    update list form on boardShow

--- app/assets/javascripts/views/board_show.js
+++ app/assets/javascripts/views/board_show.js
@@ -3,14 +3,22 @@ TrelloClone.Views.BoardShow = Backbone.View.extend({
 
   initialize: function() {
     this.listenTo(this.model, 'sync add remove', this.render);
-    this._subviews = [];
+    this.listenTo(this.model.lists(), 'sync add remove', this.render);
+    this._listSubViews = [];
+    this._form = new TrelloClone.Views.ListForm({
+      board: this.model,
+      collection: this.model.lists()
+    });
   },
 
   removeSubviews: function() {
-    this._subviews.forEach(function(subview) {
+    this._listSubViews.forEach(function(subview) {
       subview.remove();
     });
-    this._subviews = [];
+    this._listSubViews = [];
+
+    this._form.remove();
+    this._form = null;
   },
 
   remove: function() {
@@ -22,12 +30,20 @@ TrelloClone.Views.BoardShow = Backbone.View.extend({
     var content = this.template({board: this.model});
     this.$el.html(content);
     this.removeSubviews();
-    
+
+    // populate lists
     this.model.lists().each(function(list) {
       var view = new TrelloClone.Views.ListShow({model: list});
       this.$('.row').append(view.render().$el);
-      this._subviews.push(view);
+      this._listSubViews.push(view);
     }, this);
+
+    // append list form
+    this._form = new TrelloClone.Views.ListForm({
+      board: this.model,
+      collection: this.model.lists()
+    });
+    this.$('.row').append(this._form.render().$el);
 
     return this;
   }
