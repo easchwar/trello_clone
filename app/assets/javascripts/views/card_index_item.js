cecd0f61dbb9f8491d2b913e39c08816afc182ec
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-06 15:16:36 -0700

    switch listShow to composite view containing cardIndexItem views

--- app/assets/javascripts/views/card_index_item.js
+++ app/assets/javascripts/views/card_index_item.js
@@ -1,5 +1,13 @@
 TrelloClone.Views.CardIndexItem = Backbone.View.extend({
   template: JST['cards/index_item'],
 
-  
+  tagName: 'div',
+  className: 'list-group-item hoverable',
+
+  render: function() {
+    var content = this.template({card: this.model});
+    this.$el.html(content);
+    return this;
+  },
+
 });
--- app/assets/javascripts/views/list_show.js
+++ app/assets/javascripts/views/list_show.js
@@ -8,29 +8,52 @@ TrelloClone.Views.ListShow = Backbone.View.extend({
     'click .delete': 'delete'
   },
   initialize: function() {
+    this._subviews = [];
     this.listenTo(this.model, 'sync', this.render);
     this.listenTo(this.model.cards(), 'add remove', this.render);
   },
 
-  _removeFormView: function() {
-    if (this._cardForm) {
-      this._cardForm.remove();
-    }
-    this._cardForm = null;
-  },
-
-  render: function() {
-    var content = this.template({list: this.model});
-    this.$el.html(content);
-    this._removeFormView();
-
+  addFormView: function() {
     var formView = new TrelloClone.Views.CardForm({
       model: new TrelloClone.Models.Card(),
       collection: this.model.cards(),
       list: this.model,
     });
     this._cardForm = formView;
-    this.$('li.form').html(formView.render().$el);
+    this.$('div.form').html(formView.render().$el);
+  },
+
+  addIndexItems: function () {
+    var $div = this.$('div.index-items');
+    this.model.cards().each(function(card) {
+      var view = new TrelloClone.Views.CardIndexItem({model: card});
+      $div.append(view.render().$el);
+    });
+  },
+
+  removeSubViews: function() {
+    if (this._cardForm) {
+      this._cardForm.remove();
+    }
+    this._subviews.forEach(function(subview) {
+      subview.remove();
+    });
+
+    this._subviews = [];
+    this._cardForm = null;
+  },
+
+  render: function() {
+    var content = this.template({list: this.model});
+    this.$el.html(content);
+
+    // clean up old subviews
+    this.removeSubViews();
+
+    // add subviews
+    this.addFormView();
+    this.addIndexItems();
+
     return this;
   },
 
