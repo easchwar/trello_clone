4a9c2ece40f3eb144571a9b103c2bd6f56478862
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-04-06 15:50:34 -0700

    add ability to destroy cards

--- app/assets/javascripts/views/card_index_item.js
+++ app/assets/javascripts/views/card_index_item.js
@@ -4,10 +4,18 @@ TrelloClone.Views.CardIndexItem = Backbone.View.extend({
   tagName: 'div',
   className: 'list-group-item hoverable',
 
+  events: {
+    'click .card-delete': 'delete'
+  },
+
   render: function() {
     var content = this.template({card: this.model});
     this.$el.html(content);
     return this;
+  },
+
+  delete:function() {
+    this.model.destroy();
   },
 
 });
